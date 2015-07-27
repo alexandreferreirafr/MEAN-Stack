7989ccba66b7c33ec5341583c66d496bf4c7d529
Alexandre Ferreira <contact@alexandreferreira.fr>
2015-07-27 19:47:59 +0200

    improve client code

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,13 +1,12 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http) {
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvIdentity, mvNotifier, mvAuth) {
+	$scope.identity = mvIdentity;
 	$scope.signin = function(username, password) {
-		console.log('I\'m not done yet!');
-		$http.post('/login', {username: username, password: password})
-			.then(function(response){
-				if (response.data.success) {
-					console.log('login');
-				} else {
-					console.log('error');
-				}
-			});
+		mvAuth.authenticateUser(username, password).then(function(success){
+			if (success) {
+				mvNotifier.notify('You have signed in');
+			} else {
+				mvNotifier.notify('username/password incorrect');
+			}
+		});
 	};
 });
